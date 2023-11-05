8b411a48b68885537f35fa3be6f1cbb1fb674b31
Kristine Huang <kristinehuang2023@gmail.com>
2023-11-05 15:05:21 -0500

    Added countdown.js

---
+++ hab-website/src/countdown.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import App from './App';
+import { useState } from 'react';
+
+
+function Timer(){
+    <body> 
+        <div style={{width: '100%', textAlign: 'center', color: 'black', fontSize: 50, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>COUNTDOWN</div>
+        <div style={{width: '100%', height: '100%', textAlign: 'center', color: 'black', fontSize: 31, fontFamily: 'Raleway', fontWeight: '700', wordWrap: 'break-word'}}>TO OUR NEXT LAUNCH</div>
+        {/* in EST */}
+        const today = new Date();
+        const currMonth = today.getMonth();
+        const currDate = today.getDate();
+        const currHours = today.getHours();
+        const currMinutes() = today.getMinutes();
+
+        
+
+        {/* const launchDate = new Date('December 3, 2023 10:00:00');
+        const daysLeft;
+        const hoursLeft; 
+        const minutesLeft; 
+
+        if (currMonth == 1){
+            if (currYear % 4 != 0){
+                //28 days
+                daysLeft += 
+
+            }
+            else{
+                //29 days
+            }
+            
+        }
+        else if (currMonth %2 == 0  && (currMonth <= 6)){
+            //31 days
+        }
+        else if (currMonth %2 == 1  && (currMonth <= 6)){
+            //30 days
+        }
+        else if (currMonth %2 == 0  && (currMonth > 6)){
+            //30 days
+        }
+        else {
+            //31 days
+        }      */}
+
+
+        
+
+
+    </body>
+}
